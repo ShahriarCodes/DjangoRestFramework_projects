86f81217a68c7af5762e739a0f4e8e8a51316040
shahriar100 <shahriar.hasan100@gmail.com>
2020-12-01 21:19:55 +0600

    basic api testing done

---
+++ DRF_course_very_academy/react/blogapi/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
